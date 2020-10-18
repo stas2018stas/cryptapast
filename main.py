878a8c283efef696dbd1360ab781512da616d581
Stas Yaroslavtsev <yastas2018@mail.ru>
2020-10-18 18:58:41 +0700

    Fix bugs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from SqliteManager import SqliteManager as sm
 
 sm.connectToDatabase("data.db")
 
-print("CRYPTAPAST V0.0.1.0")
+print("CRYPTAPAST V0.1.0.0")
 file = input("Enter file name(without .aes if you want to decrypt): ")
 want = input("if you want encrypt your file, write 'e' without brackets or you can write 'd' to perform the opposite action")
 if(want == "e"):
